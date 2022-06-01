149f076f82502600f84ba72f4021035d6c1e26c5
Sadeemfolklore <106690733+Sadeemfolklore@users.noreply.github.com>
2022-06-01 19:06:09 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("left")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Right ")
+})
+basic.showString("Sadeem")
